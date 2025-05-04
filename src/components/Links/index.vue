2122cec8b3860240f3df4b988cf645d725275f15
ImCaiYi <badcaiyi@qq.com>
2025-05-04 20:36:59 +0800

    Create index.vue

---
+++ src/components/Links/index.vue
@@ -0,0 +1,27 @@
+// 可前往 https://www.xicons.org 自行挑选并在此处引入
+// 此处引入的是 fa 类型
+import {
+  Link,
+  Blog,
+  CompactDisc,
+  Cloud,
+  Compass,
+  Book,
+  Fire,
+  LaptopCode,
+  AttachEmailOutlined,
+} from "@vicons/fa";
+
+...
+
+// 网站链接图标
+const siteIcon = {
+  Blog,
+  Cloud,
+  CompactDisc,
+  Compass,
+  Book,
+  Fire,
+  LaptopCode,
+  AttachEmailOutlined,
+};
