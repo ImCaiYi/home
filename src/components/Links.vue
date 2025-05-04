ad26325b7e69fdf68adbccb47b99f1af46d36435
ImCaiYi <badcaiyi@qq.com>
2025-05-04 14:17:51 +0000

    114514

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,11 +43,12 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, AttachEmailOutlined, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
 import siteLinks from "@/assets/siteLinks.json";
+import { AttachEmailOutlined } from "@vicons/material";
 
 const store = mainStore();
 
@@ -70,6 +71,7 @@ const siteIcon = {
   AttachEmailOutlined,
   Fire,
   LaptopCode,
+  Book,
 };
 
 // 链接跳转
