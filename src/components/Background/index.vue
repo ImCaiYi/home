95e62bd9f0b367c50c62eb435215050cf55af725
ImCaiYi <badcaiyi@qq.com>
2025-05-04 21:21:13 +0800

    Update index.vue

--- src/components/Background/index.vue
+++ src/components/Background/index.vue
@@ -1,4 +1,4 @@
 if (type == 0) {
   // 修改此处 Math.random() 后面的第一个数字为图片的数量
-  bgUrl.value = `/images/background${Math.floor(Math.random() * 1 + 1)}.webp`;
+  bgUrl.value = `/images/background1.jpg`;
 }
