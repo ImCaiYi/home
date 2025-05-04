e043bac48ff9b1a6160d94ba3e7d0255143d5c1d
ImCaiYi <badcaiyi@qq.com>
2025-05-04 21:45:17 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,AttachEmailOutlined } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, AttachEmailOutlined, Fire, LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -67,10 +67,9 @@ const siteIcon = {
   Cloud,
   CompactDisc,
   Compass,
-  Book,
+  AttachEmailOutlined,
   Fire,
   LaptopCode,
-  AttachEmailOutlined,
 };
 
 // 链接跳转
